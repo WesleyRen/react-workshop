1f1f1288876f81ad7822f5ab2571e98583456939
Wesley Ren <wren@walmartlabs.com>
2018-07-27 20:43:14 -0700

    added basic stuff, not redirect yet.

--- subjects/18-Mini-Router/exercise/mini-router.js
+++ subjects/18-Mini-Router/exercise/mini-router.js
@@ -17,33 +17,65 @@ history.listen(() => {
 history.push('/something')
 */
 
+const RouterContext = React.createContext();
+
 class Router extends React.Component {
   history = createHashHistory();
 
+  state = {location: this.history.location};
+
+  handleOnClick = (to) => {
+      this.history.push(to);
+      this.setState({location: this.history.location});
+  };
+
   render() {
-    return this.props.children;
+    return <RouterContext.Provider {...this.props} value={{
+        _onClick: this.handleOnClick,
+        location: this.state.location,
+    }}>
+       {this.props.children}
+    </RouterContext.Provider>
   }
 }
 
 class Route extends React.Component {
-  render() {
-    const { path, render, component: Component } = this.props;
-    return null;
-  }
+    render(){
+        const {path, render, component: Component} = this.props;
+        console.log(path);
+        return <RouterContext.Consumer>
+            {router => {
+                if (router.location.pathname.startsWith(path)) {
+                    if (render) {
+                        return render();
+                    }
+                    if (Component) {
+                        return <Component/>;
+                    }
+                }
+                return null;
+            }}
+        </RouterContext.Consumer>
+    };
 }
 
 class Link extends React.Component {
-  handleClick = event => {
-    event.preventDefault();
-  };
+    handleOnClick = (event, router) => {
+        event.preventDefault();
+        router._onClick(this.props.to);
+    };
 
-  render() {
-    return (
-      <a href={`#${this.props.to}`} onClick={this.handleClick}>
-        {this.props.children}
-      </a>
-    );
-  }
+    render() {
+        const {children, to} = this.props;
+        return (
+            <RouterContext.Consumer>
+                {router =>  <a href={`#${to}`} onClick={(event) => this.handleOnClick(event, router)}>
+                    {children}
+                </a>}
+            </RouterContext.Consumer>
+        );
+    }
+
 }
 
 export { Router, Route, Link };
